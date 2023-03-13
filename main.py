4f38dc7b40fa3e2b84881172abd5d9e75cc06775
sonsohwi <ksl428302@gmail.com>
2023-03-13 14:03:02 +0900

    feat: rock scissors paper game 주석

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import random
 
-rsp = ['가위', '바위', '보']
-result = {0: '비김', 1: '이김', 2: '짐'}
+rsp = ['가위', '바위', '보']  #list에 가위 바위 보 저장
+result = {0: '비김', 1: '이김', 2: '짐'} #딕셔너리에 승패 저장
 
-def check(user, com):
+def check(user, com): # 승패를 확인하는 함수
 
-    print('컴퓨터:', com)
+    print('컴퓨터:', com) # 컴퓨터의 랜덤 값 출력
 
     if user == com:
         print(result.get(0))
@@ -24,20 +24,18 @@ print('게임을 끝내려면 "끝"을 입력하세요\n')
 
 while True:
     user = input('입력해주세요>> ')
-    if user == '끝':
+    if user == '끝': #유저가 끝을 입력한 경우
         print('게임을 이용해주셔서 감사합니다 ^__^')
         print('-----------------------------------------\n')
         break
 
-    elif not user in rsp:
+    elif not user in rsp: # rsp 리스트에 없는 값을 입력했을 경우
         print('다시 입력해주세요')
         print('-----------------------------------------\n')
         continue
 
-    com = random.randint(0,2)
-    com = rsp[com]
+    com = random.randint(0,2) #랜덤 값 0~2중 하나 부여
+    com = rsp[com] #받은 값을 리스트의 값으로 변환
 
-    check(user, com)
+    check(user, com) #check함수 호출
     print('-----------------------------------------\n')
-
-#print('-----------------------------------------\n')
